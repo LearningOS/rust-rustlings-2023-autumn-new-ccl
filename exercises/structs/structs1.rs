636f179009681e219c47351aff28b4c418b443d4
new-ccl <113997248+new-ccl@users.noreply.github.com>
2023-10-11 20:11:16 +0800

    10.1!

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -9,12 +9,17 @@
 
 struct ColorClassicStruct {
     // TODO: Something goes here
+    red:i32,
+    green:i32,
+    blue:i32
 }
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct(i32,i32,i32);
 
 #[derive(Debug)]
-struct UnitLikeStruct;
+struct UnitLikeStruct{
+    name:String,
+}
 
 #[cfg(test)]
 mod tests {
@@ -23,7 +28,9 @@ mod tests {
     #[test]
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
-        // let green =
+        let green =ColorClassicStruct{red:0,
+            green:255,
+            blue:0};
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -33,7 +40,7 @@ mod tests {
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
-        // let green =
+        let green =ColorTupleStruct(0,255,0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -42,8 +49,7 @@ mod tests {
 
     #[test]
     fn unit_structs() {
-        // TODO: Instantiate a unit-like struct!
-        // let unit_like_struct =
+        // TODO: Instantiate a unit-like struct!:
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
