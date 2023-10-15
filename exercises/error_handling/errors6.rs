2644718ec8c48c40b378b9b9496d85c1b8aa9893
new-ccl <113997248+new-ccl@users.noreply.github.com>
2023-10-15 14:50:51 +0800

    10.15

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -9,9 +9,9 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::num::ParseIntError;
+
+use std::{num::ParseIntError, i64};
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
 #[derive(PartialEq, Debug)]
@@ -24,6 +24,10 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
+
+    fn from_parse_int(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
     // TODO: add another error conversion function here.
     // fn from_parseint...
 }
@@ -31,8 +35,16 @@ impl ParsePosNonzeroError {
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
+    match s.parse::<i64>() {
+        Ok(value) => {
+            match PositiveNonzeroInteger::new(value) {
+                Ok(num) => Ok(num),
+                Err(err) => Err(ParsePosNonzeroError::from_creation(err)),
+            }
+        }
+        Err(err) => Err(ParsePosNonzeroError::from_parse_int(err)),
+    }
+    
 }
 
 // Don't change anything below this line.
--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,7 +6,7 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 // Step 1.
 // Complete the `capitalize_first` function.
@@ -15,7 +15,11 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => {
+            let first_char=first.to_ascii_uppercase();
+            let rest_char:String=c.collect();
+            return format!("{}{}",first_char,rest_char);
+        },
     }
 }
 
@@ -24,7 +28,7 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    words.iter().map(|x| capitalize_first(x)).collect()
 }
 
 // Step 3.
@@ -32,7 +36,13 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let concatenated: String = words.iter().fold(String::new(), |mut acc, &s| {
+        if !words.is_empty() {
+            acc.push_str(&capitalize_first(s));
+        }
+        acc
+    });
+    concatenated
 }
 
 #[cfg(test)]
